0e63ccdf1badd2623f23b761794721883b2c5d8c
joão pedro <joaopedro251900@gmail.com>
2024-09-21 16:35:55 -0300

    footer

--- JS/script.js
+++ JS/script.js
@@ -14,6 +14,54 @@ document.addEventListener('DOMContentLoaded', function() {
     typeWriter();  // Inicia a animação de digitação
 });
 
+/*BTN sound*/ 
+
 document.getElementById('clickButton').addEventListener('click', function() {
+    const apresentacao = document.getElementById("apresentacao")
+    const TitlePy = document.getElementById("TitlePy")
+    const dinossauroBK = document.getElementById("BK")
+    const OrientadoAEvento1 = document.getElementById("OrientadoAEvento1")
+    const OrientadoAEvento2 = document.getElementById("OrientadoAEvento2")
+    if(apresentacao.classList == "apresentacaoDark"){
+        apresentacao.classList.toggle("apresentacaoDark");
+        apresentacao.classList.toggle("apresentacao")
+        TitlePy.classList.toggle("titleOEDark");
+        TitlePy.classList.toggle("titleOE");
+        dinossauroBK.classList.toggle("dinossauroBKDark");
+        dinossauroBK.classList.toggle("dinossauroBK");
+        OrientadoAEvento1.style.opacity = '1';
+        OrientadoAEvento2.style.opacity = '0';
+        document.getElementById("btnNextDark").style.display = 'none'
+        document.querySelector('.linkNext').style.display = 'flex'
+    }
+    document.getElementById('clickAudio').play();
+});
+
+/*Mudar BackGround*/ 
+document.querySelector('.linkNext').addEventListener('click', function(e) {
+    e.preventDefault();  // Prevents the default link behavior
+    const apresentacao = document.getElementById("apresentacao")
+    const TitlePy = document.getElementById("TitlePy")
+    const dinossauroBK = document.getElementById("BK")
+    const OrientadoAEvento1 = document.getElementById("OrientadoAEvento1")
+    const OrientadoAEvento2 = document.getElementById("OrientadoAEvento2")
+    if(apresentacao.classList == "apresentacao"){
+        apresentacao.classList.toggle("apresentacaoDark");
+        apresentacao.classList.toggle("apresentacao")
+        TitlePy.classList.toggle("titleOEDark");
+        TitlePy.classList.toggle("titleOE");
+        dinossauroBK.classList.toggle("dinossauroBKDark");
+        dinossauroBK.classList.toggle("dinossauroBK");
+        OrientadoAEvento1.style.opacity = '0';
+        OrientadoAEvento2.style.opacity = '1';
+        document.getElementById("btnNextDark").style.display = 'flex'
+        document.querySelector('.linkNext').style.display = 'none'
+    }
+    document.getElementById('clickAudio').play();
+});
+
+
+document.querySelector('.linkNextDark').addEventListener('click', function(e) {
+
     document.getElementById('clickAudio').play();
 });
